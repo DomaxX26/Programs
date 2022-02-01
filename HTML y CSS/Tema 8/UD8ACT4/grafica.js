a9bda5a1616ecc5e37cf2f48adbaddc2a3cc220d
Joan Pons <joan.ponssanchis@gmail.com>
2022-02-01 13:11:13 +0100

    Boostrap

---
+++ HTML y CSS/Tema 8/UD8ACT4/grafica.js
@@ -0,0 +1,28 @@
+window.onload = main;
+
+function main(){
+    new Chart(document.getElementById("pie-chart"), {
+        type: "pie",
+        data: {
+            labels: ["Africa", "Asia", "America"],
+            datasets: [
+                {
+                    label: "Population (millions)",
+                    backgroundColor: [
+                        "#3e95cd",
+                        "#8e5ea2",
+                        "#3cba9f",
+                    ],
+                    data: [150, 300, 50]
+                }
+            ]
+        },
+        options: {
+            title: {
+                display: true,
+            }
+        }
+    });
+    
+}
+
