865ef51bdebf82083b7c13fa7efa64ed10d66ca7
Joan Pons <joan.ponssanchis@gmail.com>
2022-02-03 11:19:00 +0100

    Boostrap

--- HTML y CSS/Tema 8/UD8ACT4/grafica.js
+++ HTML y CSS/Tema 8/UD8ACT4/grafica.js
@@ -1,6 +1,6 @@
 window.onload = main;
 
-function main(){
+function main() {
     new Chart(document.getElementById("pie-chart"), {
         type: "pie",
         data: {
@@ -8,25 +8,27 @@ function main(){
             datasets: [
                 {
                     label: "Population (millions)",
-                    backgroundColor: [
-                        "#3e95cd",
-                        "#8e5ea2",
-                        "#3cba9f",
-                    ],
+                    backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f"],
                     data: [150, 300, 50]
                 }
             ]
         },
         options: {
             title: {
-                display: true,
+                display: true
             }
         }
     });
-
-    document.getElementById('escarabajo').addEventListener('click', function (event) {
-        document.getElementById("myModal").style = "display : block;"
-    })
-    
+    document.getElementById("escarabajo").addEventListener("click", mostrarOcultar);
+    document.getElementById("myModal").addEventListener("click", mostrarOcultar);
 }
 
+
+function mostrarOcultar(){
+    let modal = document.getElementById('myModal');
+    if(modal.style.display === "block"){
+        modal.style.display = "none";
+    }else{
+        modal.style.display = "block";
+    }
+}
