0897baaaa5b59c14a8cba400ef4051981299d4cf
Joan Pons <joan.ponssanchis@gmail.com>
2022-02-24 21:40:34 +0100

    login

--- JavaScript/React/proyecto/src/Components/Login.jsx
+++ JavaScript/React/proyecto/src/Components/Login.jsx
@@ -1,10 +1,95 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Button, Form, Container } from 'react-bootstrap';
+import { useFormik } from "formik";
+import * as Yup from 'yup';
+import { useNavigate } from 'react-router-dom';
 
-export const Login = () => {
+
+const SignupSchema = Yup.object().shape({
+  email: Yup.string().email('Email Invalido').required('Campo Obligatorio'),
+  password: Yup.string().min(6, 'Muy Corto!').max(20, 'Muy Largo!').required('Campo Obligatorio'),
+});
+
+
+function Login() {
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+
+
+  const formik = useFormik({
+    initialValues: { email: '',
+    password: "" },
+    onSubmit: values => {
+      fetch("https://api.tendaciclista.ccpegoilesvalls.es/api/login", {
+        method: 'POST',
+        body: JSON.stringify(values, null, 2),
+        headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        },
+        mode: 'cors',
+      }).then(response => response.json())
+        .then(data => {
+          if (data.error != null) {
+            console.log(data.error);
+            setError(data.error);
+          } else {
+            console.error("succes", data.data.token); 
+            localStorage.setItem("tk", JSON.stringify(data.data.token));
+            navigate('/');
+          }
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+          setError(error)
+
+        });
+    },
+    validationSchema: SignupSchema,
+  });
+
   return (
-    <div>Login</div>
-  )
-  
-}
+    <div>
+      <h1>Login</h1>
+      <div className="text-danger">{error}</div>
+      <Container>
+      <Form onSubmit={formik.handleSubmit}>
+        <Form.Group className="mb-3" >
+          <Form.Label htmlFor="email" className='d-flex justify-content-start'>Correo Electrónico</Form.Label>
+          <Form.Control
+            type="email"
+            id="email"
+            name="email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
 
-export default Login;
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <div className="text-danger d-flex justify-content-start">{formik.errors.email}</div>) : null}
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label className='d-flex justify-content-start'>Contraseña</Form.Label>
+          <Form.Control
+            type="password"
+            id="password"
+            name="password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
+          />
+          {formik.touched.password && formik.errors.password ? (
+            <div className="text-danger d-flex justify-content-start">{formik.errors.password}</div>) : null}
+        </Form.Group>
+        <Form.Group className="mb-3" >
+          <Button variant="primary" type="submit"> Enviar </Button>
+        </Form.Group>
+
+      </Form>
+      </Container>
+
+    </div>
+  );
+}
+export default Login; 
