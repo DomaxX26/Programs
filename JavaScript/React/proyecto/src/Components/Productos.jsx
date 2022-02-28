028193175f7c95340f8da8ee78549e0e9c7d4108
Joan Pons <joan.ponssanchis@gmail.com>
2022-02-28 20:58:47 +0100

    React

--- JavaScript/React/proyecto/src/Components/Productos.jsx
+++ JavaScript/React/proyecto/src/Components/Productos.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Row, Col, Button, Table } from 'react-bootstrap';
+import { Container, Row, Col, Button, Table, Modal, ModalBody, ModalFooter } from 'react-bootstrap';
+import ModalHeader from 'react-bootstrap/esm/ModalHeader';
 import * as yup from 'yup';
 import { useFormik } from "formik";
 import { useNavigate } from 'react-router-dom';
@@ -12,14 +13,15 @@ const productos = yup.object().shape({
 
 function Productos() {
 
+    const [isModalOpen, setIsModalOpen] = useState(false);
     const [data, setData] = useState([]);
-    const [error, setError] = useState("");
+    const [error] = useState("");
     const navigate = useNavigate();
 
     useEffect(() => {
         validar();
         catchProduct();
-    }, []);
+    });
 
     function validar() {
         var token = JSON.parse(localStorage.getItem("tk"))
@@ -77,41 +79,39 @@ function Productos() {
             })
     }
 
-    const editProduct = () => {
-        const tallas = formik.values.tallas.split(" ");
-
-        const putData = {
+    const editProduct = (evt) => {
+        evt.preventDefault();
+        let tallas = formik.values.tallas.trim().split(',');
+        let product = {
             'nombre': formik.values.nombre,
             'precio': formik.values.precio,
             'tallas': tallas
-        };
+        }
 
-        fetch("https://api.tendaciclista.ccpegoilesvalls.es/api/productos"+'/'+formik.values._id, {
+        fetch('https://api.tendaciclista.ccpegoilesvalls.es/api/productos/' + formik.values.id, {
             method: 'PUT',
-            body: JSON.stringify(putData),
             headers: {
-                'Accept': 'application/json, text/plain, */*',
                 'Content-Type': 'application/json',
-                "auth-token": JSON.parse(localStorage.getItem("tk"))
+                'Accept': 'application/json',
+                'auth-token': JSON.parse(localStorage.getItem("tk")),
             },
-        }).then(response => response.json())
-            .then(datos => {
-                console.log(datos);
+            body: JSON.stringify(product)
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+
             })
-            .catch((error) => {
-                console.error('Error:', error);
-                setError(error);
-            });
     }
 
-    const valuesEdit = (id) => {
-        const element = data.find(item => item._id === id); //Buscar el mismo id del array
-        let tallasString = " ";
-        formik.values._id = id;
-        formik.values.nombre = element.nombre;
-        formik.values.precio = element.precio;
-        element.tallas.forEach(itm => { tallasString = tallasString + itm + ' ' })
-        formik.values.tallas = tallasString;
+    const valuesEdit = (producto) => {
+        const talla = producto.tallas.toString();
+        
+        formik.values.id = producto._id;
+        formik.values.nombre = producto.nombre;
+        formik.values.precio = producto.precio;
+        formik.values.tallas = talla
+        setIsModalOpen(true);
     }
 
 
@@ -144,7 +144,7 @@ function Productos() {
                                             producto.tallas.map(talla => talla + " ")
                                         }</td>
                                         <td>
-                                            <button className="btn btn-primary mx-2" data-bs-toggle="modal" data-bs-target="#modalEditProduct" onClick={() => { valuesEdit(producto._id) }} ><i className="bi bi-pencil-square"></i></button>
+                                            <button className="btn btn-primary mx-2" data-bs-toggle="modal" data-bs-target="#modalEditProduct" onClick={() => { valuesEdit(producto) }} ><i className="bi bi-pencil-square"></i></button>
                                             <button className="btn btn-danger" onClick={() => { console.log("Producto a borrar: " + producto.nombre) }}><i className="bi bi-trash"></i></button>
                                         </td>
                                     </tr>
@@ -209,61 +209,62 @@ function Productos() {
                         </div>
                     </div>
                     {/*MODAL EDIT PRODUCT*/}
-                    <div className="modal fade" id="modalEditProduct" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div className="modal-dialog">
-                            <div className="modal-content">
-                                <div className="modal-header">
-                                    <h5 className="modal-title" id="exampleModalLabel">Actualizar Producto</h5>
-                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div className="modal-body">
-                                    <div className="text-danger">{error}</div>
-                                    <div className="form-group">
-                                        <input className="form-control"
-                                            type="hidden"
-                                            name="_id"
-                                            id="_id"
-                                            value={formik.values._id} />
-                                        <br />
-                                        <label htmlFor="nombre" className='d-flex justify-content-start'>Nombre</label>
-                                        <input className="form-control"
-                                            type="text"
-                                            name="nombre"
-                                            id="nombre"
-                                            onBlur={formik.handleBlur}
-                                            onChange={formik.handleChange}
-                                            value={formik.values.nombre}
-                                        />
-                                        {formik.touched.nombre && formik.errors.nombre ? (<div className="text-danger d-flex justify-content-start">{formik.errors.nombre}</div>) : null}
-                                        <br />
-                                        <label htmlFor="precio" className='d-flex justify-content-start'>Precio</label>
-                                        <input className="form-control"
-                                            type="number"
-                                            name="precio"
-                                            id="precio"
-                                            onBlur={formik.handleBlur}
-                                            onChange={formik.handleChange}
-                                            value={formik.values.precio} />
-                                        {formik.touched.precio && formik.errors.precio ? (<div className="text-danger d-flex justify-content-start">{formik.errors.precio}</div>) : null}
-                                        <br />
-                                        <label htmlFor="imagen" className='d-flex justify-content-start'>Tallas</label>
-                                        <input className="form-control"
-                                            type="text"
-                                            name="tallas"
-                                            id="tallas"
-                                            onBlur={formik.handleBlur}
-                                            onChange={formik.handleChange}
-                                            value={formik.values.tallas} />
-                                        {formik.touched.tallas && formik.errors.tallas ? (<div className="text-danger d-flex justify-content-start">{formik.errors.tallas}</div>) : null}
-                                    </div>
-                                </div>
-                                <div className="modal-footer">
-                                    <button type="button" className="btn btn-primary" onClick={() => editProduct()}>Actualizar</button>
-                                    <button type="button" className="btn btn-primary">Cancelar</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <Modal show={isModalOpen} onClose={() => setIsModalOpen(false)}>
+                        <ModalHeader>
+                            <h1>Editar Producte</h1>
+                            <Button variant='secondary' onClick={() => setIsModalOpen(false)}>
+                                <i className="bi bi-x"></i>
+                            </Button>{' '}
+                        </ModalHeader>
+                        <ModalBody>
+                            <form>
+                                {/* Camp del nom del producte */}
+                                <label htmlFor="nombre" className='mb-2'>Nom del producte</label>
+                                <input
+                                    type="text"
+                                    name="nombre"
+                                    className="form-control"
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.nombre}
+                                />
+                                {formik.touched.nombre && formik.errors.nombre ? (
+                                    <div className="text-danger">{formik.errors.nombre}</div>) : null}
+                                {/* Camp del preu del producte */}
+                                <label htmlFor="precio" className='mt-2 mb-2'>Preu del producte</label>
+                                <input
+                                    type="number"
+                                    name="precio"
+                                    className="form-control"
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.precio}
+                                />
+                                {formik.touched.precio && formik.errors.precio ? (
+                                    <div className="text-danger">{formik.errors.precio}</div>) : null}
+                                <label htmlFor="talla" className='mt-2 mb-2'>Talla del producte</label>
+                                <input
+                                    type="text"
+                                    name="tallas"
+                                    className="form-control"
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.tallas}
+                                />
+                                {formik.touched.tallas && formik.errors.tallas ? (
+                                    <div className="text-danger">{formik.errors.tallas}</div>) : null}
+                                <button className='btn btn-primary' onClick={(evt) => editProduct(evt)}>
+                                    Actualizar
+                                </button>
+                                <button className='btn btn-secondary' type="button" onClick={(evt) => { evt.preventDefault(); setIsModalOpen(false) }}>
+                                    Cancelar
+                                </button>
+                            </form>
+                        </ModalBody>
+                        <ModalFooter>
+                        </ModalFooter>
+
+                    </Modal>
                 </Col>
             </Row>
         </Container>
